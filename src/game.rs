aeaf4d0bb5e947ff64a3e385012e134abfa100a7
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-02-01 19:20:14 +0000

    Added graphics, flocking behaviour

--- src/game.rs
+++ src/game.rs
@@ -3,12 +3,13 @@ use specs::prelude::*;
 use quicksilver::{
     geom::{Rectangle, Triangle, Vector, Transform},
     input::Key,
-    graphics::Color,
-    lifecycle::{Window, Event},
+    graphics::{Color, Background, Image},
+    lifecycle::{Window, Event, Asset},
 };
+use rand::{thread_rng, prelude::*};
 use crate::{
     State,
-    world::{self, Pos, Ori, Seafloor},
+    world::{self, Pos, Ori, Vel, Body, Seafloor},
 };
 
 #[derive(Copy, Clone, Default)]
@@ -22,6 +23,11 @@ pub struct Game {
     world: world::World,
     globals: world::Globals,
     inputs: Inputs,
+    time: f32,
+
+    background: Asset<Image>,
+    submarine: Asset<Image>,
+    seal: Asset<Image>,
 }
 
 impl Game {
@@ -31,17 +37,23 @@ impl Game {
             world,
             globals,
             inputs: Inputs::default(),
+            time: 0.0,
+            background: Asset::new(Image::load("ocean.png")),
+            submarine: Asset::new(Image::load("submarine.png")),
+            seal: Asset::new(Image::load("seal.png")),
         }
     }
 
     pub fn tick(&mut self, window: &mut Window) {
+        let time = self.time;
+
         // Handle input
         self.inputs.left = window.keyboard()[Key::Left].is_down();
         self.inputs.right = window.keyboard()[Key::Right].is_down();
         self.inputs.boost = window.keyboard()[Key::Up].is_down();
 
         // Tick world
-        let tick_info = world::tick(&self.world, self.inputs);
+        let tick_info = world::tick(&self.world, self.inputs, time);
 
         let world_trans = Transform::IDENTITY
             * Transform::translate((Vec2::new(window.screen_size().x, window.screen_size().y) * 0.5).into_tuple())
@@ -49,6 +61,22 @@ impl Game {
             * Transform::translate((-tick_info.view_centre).into_tuple());
 
         window.clear(Color::WHITE);
+
+        // Background
+        self.background.execute(|background| {
+            for i in 0..20 {
+                let w = 512.0;
+                let x = (((tick_info.view_centre.x - 1000.0) / w).floor() + i as f32) * w;
+                window.draw_ex(
+                    &Rectangle::new((x, -320.0), (w, 2048.0)),
+                    Background::Img(&background),
+                    world_trans,
+                    -2.0,
+                );
+            }
+
+            Ok(())
+        });
 
         // Sea floor
         let seafloor = self.world.read_resource::<Seafloor>();
@@ -68,19 +96,48 @@ impl Game {
         }
 
         // Entities
-        for (pos, ori) in (
+        for (pos, ori, vel, body) in (
             &self.world.read_storage::<Pos>(),
             &self.world.read_storage::<Ori>(),
+            &self.world.read_storage::<Vel>(),
+            &self.world.read_storage::<Body>(),
         ).join() {
-            let rect = Rectangle::new(Vec2::new(-42.0, -12.0).into_tuple(), Vec2::new(64.0, 24.0).into_tuple());
-            window.draw_ex(
-                &rect,
-                Color::RED,
-                world_trans
-                    * Transform::translate(pos.0.into_tuple())
-                    * Transform::rotate(ori.0 * 180.0 / 3.1415),
-                0.0,
-            );
+            match body {
+                Body::Seal | Body::Fish => {
+                    self.seal.execute(|seal| {
+                        window.draw_ex(
+                            &Rectangle::new((-48.0, -32.0), (64.0, 64.0)),
+                            Background::Img(&seal),
+                                world_trans
+                                * Transform::translate(pos.0.into_tuple())
+                                * Transform::rotate(ori.0 * 180.0 / 3.1415)
+                                * Transform::scale(if vel.0.x > 0.0 { (1.0, 1.0) } else { (1.0, -1.0) }),
+                            0.0,
+                        );
+
+                        Ok(())
+                    });
+                },
+                Body::Submarine => {
+                    self.submarine.execute(|submarine| {
+                        window.draw_ex(
+                            &Rectangle::new((-512.0, -512.0), (1024.0, 1024.0)),
+                            Background::Img(&submarine),
+                            world_trans
+                                * Transform::rotate((time * 1.0).sin() * 3.0)
+                                * Transform::translate((
+                                    pos.0 + Vec2::new(
+                                        thread_rng().gen_range(-1.0, 1.0),
+                                        thread_rng().gen_range(-1.0, 1.0) + (time * 2.0).sin() * 16.0,
+                                    )
+                                ).into_tuple()),
+                            -0.5,
+                        );
+
+                        Ok(())
+                    });
+                },
+            }
         }
 
         // Sea
@@ -90,5 +147,7 @@ impl Game {
             world_trans,
             1.0,
         );
+
+        self.time = time + 1.0 / 60.0;
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -10,6 +10,7 @@ pub use specs::World;
 
 pub struct Globals {
     pub player: Entity,
+    pub submarine: Entity,
 }
 
 pub fn create() -> (Globals, specs::World) {
@@ -20,19 +21,30 @@ pub fn create() -> (Globals, specs::World) {
     world.register::<Ori>();
     world.register::<Rot>();
     world.register::<Agent>();
+    world.register::<Body>();
 
     world.insert(Seafloor::sine());
 
     let player = world
         .create_entity()
-        .with(Pos(Vec2::broadcast(64.0)))
+        .with(Pos(Vec2::broadcast(0.0)))
         .with(Vel(Vec2::unit_x()))
         .with(Ori(0.0))
         .with(Rot(0.0))
         .with(Agent::Player)
+        .with(Body::Seal)
         .build();
 
-    for _ in 0..100 {
+    let submarine = world
+        .create_entity()
+        .with(Pos(Vec2::new(0.0, 512.0)))
+        .with(Vel(Vec2::unit_x()))
+        .with(Ori(0.0))
+        .with(Rot(0.0))
+        .with(Body::Submarine)
+        .build();
+
+    for _ in 0..30 {
         world
             .create_entity()
             .with(Pos(Vec2::new(
@@ -43,11 +55,13 @@ pub fn create() -> (Globals, specs::World) {
             .with(Ori(0.0))
             .with(Rot(0.0))
             .with(Agent::Fish)
+            .with(Body::Fish)
             .build();
     }
 
     (Globals {
         player,
+        submarine,
     }, world)
 }
 
@@ -61,7 +75,7 @@ const TURN_RATE_WATER: f32 = 0.0065;
 const TURN_RATE_AIR: f32 = 0.001;
 const GRAVITY: f32 = 0.1;
 
-pub fn tick(world: &specs::World, inputs: Inputs) -> TickInfo {
+pub fn tick(world: &specs::World, inputs: Inputs, time: f32) -> TickInfo {
     let mut tick_info = TickInfo::default();
 
     let underwater = |pos: &Pos| pos.0.y > 0.0;
@@ -102,9 +116,18 @@ pub fn tick(world: &specs::World, inputs: Inputs) -> TickInfo {
         pos.0.y = pos.0.y.min(seafloor.sample(pos.0.x));
     }
 
+    // Find all positions and velocities
+    let mut all_pos_vel = (
+        &world.read_storage::<Pos>(),
+        &world.read_storage::<Vel>(),
+    )
+        .join()
+        .map(|(p, v)| (p.0, v.0))
+        .collect::<Vec<_>>();
+
     // Control
     for (pos, vel, ori, rot, agent) in (
-        &mut world.write_storage::<Pos>(),
+        &world.read_storage::<Pos>(),
         &mut world.write_storage::<Vel>(),
         &mut world.write_storage::<Ori>(),
         &mut world.write_storage::<Rot>(),
@@ -127,7 +150,22 @@ pub fn tick(world: &specs::World, inputs: Inputs) -> TickInfo {
                 tick_info.view_centre = pos.0;
                 tick_info.view_scale = 1.0;
             },
-            Agent::Fish => rot.0 += thread_rng().gen_range(-1.0, 1.0) * TURN_RATE_WATER,
+            Agent::Fish => {
+                all_pos_vel.sort_by_key(|(other_pos, _)| pos.0.distance_squared(*other_pos) as i32);
+
+                let (total_vel, n) = all_pos_vel.iter().take(6).fold((Vec2::zero(), 0.0), |(tv, n), (_, v)| (tv + v, n + 1.0));
+                let avg_vel = total_vel / n;
+
+                let (total_pos, n) = all_pos_vel.iter().take(6).fold((Vec2::zero(), 0.0), |(tp, n), (p, _)| (tp + p, n + 1.0));
+                let avg_pos = total_pos / n;
+
+                let shy_dir = all_pos_vel.iter().take(6).fold(Vec2::zero(), |a, (p, _)| a + (pos.0 - p).try_normalized().unwrap_or(Vec2::zero()) * (100.0 - (pos.0 - p).magnitude()).max(0.0));
+                let dir = Vec2::lerp((avg_pos - pos.0).normalized(), avg_vel, 0.5) + shy_dir;
+                let dir = (dir.map(|e| e + thread_rng().gen_range(-0.02, 0.02)) - pos.0 * 0.0005).normalized();
+
+                let dir = Lerp::lerp(Vec2::new(ori.0.cos(), ori.0.sin()), dir, 0.1).try_normalized().unwrap_or(Vec2::zero());
+                ori.0 = dir.y.atan2(dir.x);
+            },
         }
 
         // Swimming
@@ -136,6 +174,8 @@ pub fn tick(world: &specs::World, inputs: Inputs) -> TickInfo {
                 ori.0.cos(),
                 ori.0.sin(),
             ) * 0.25;
+
+            rot.0 += (time * 10.0).sin() * vel.0.magnitude().sqrt() * 0.002;
         }
     }
 
@@ -166,6 +206,16 @@ impl Component for Rot {
     type Storage = VecStorage<Self>;
 }
 
+pub enum Body {
+    Seal,
+    Fish,
+    Submarine,
+}
+
+impl Component for Body {
+    type Storage = VecStorage<Self>;
+}
+
 pub enum Agent {
     Player,
     Fish,
@@ -179,7 +229,7 @@ pub struct Seafloor {
     heights: Vec<f32>,
 }
 
-const SEAFLOOR_HEIGHT: f32 = 1000.0;
+const SEAFLOOR_HEIGHT: f32 = 1500.0;
 const SEAFLOOR_STRIDE: f32 = 10.0;
 const SEAFLOOR_OFFSET: i32 = 500;
 
